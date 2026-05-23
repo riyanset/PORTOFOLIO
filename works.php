e82379867792231c831ff8303eee6dfe01fbaa02
Riyan Setiawan <riyanstwn11@@gmail.com>
2026-05-23 22:40:02 +0700

    sinkronisasi folder gambar dan kode galeri

--- works.php
+++ works.php
@@ -114,123 +114,123 @@ include 'includes/header.php';
             <h3 class="gallery-heading">FOTOGRAFI</h3>
             <div class="gallery-grid">
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_3025 (1).jpg" alt="Fotografi 1" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_3025 (1).jpg" alt="Fotografi 1" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 1</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_3027.jpg" alt="Fotografi 2" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_3027.jpg" alt="Fotografi 2" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 2</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_3029.jpg" alt="Fotografi 3" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_3029.jpg" alt="Fotografi 3" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 3</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_3034 (1).jpg" alt="Fotografi 4" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_3034 (1).jpg" alt="Fotografi 4" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 4</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_3044.jpg" alt="Fotografi 5" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_3044.jpg" alt="Fotografi 5" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 5</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_3045.jpg" alt="Fotografi 6" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_3045.jpg" alt="Fotografi 6" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 6</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_3047 (1).jpg" alt="Fotografi 7" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_3047 (1).jpg" alt="Fotografi 7" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 7</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_3049.jpg" alt="Fotografi 8" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_3049.jpg" alt="Fotografi 8" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 8</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_5540.jpg" alt="Fotografi 9" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_5540.jpg" alt="Fotografi 9" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 9</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_5541.jpg" alt="Fotografi 10" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_5541.jpg" alt="Fotografi 10" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 10</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_5558.jpg" alt="Fotografi 11" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_5558.jpg" alt="Fotografi 11" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 11</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_5574.jpg" alt="Fotografi 12" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_5574.jpg" alt="Fotografi 12" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 12</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/_MG_5615 (1).jpg" alt="Fotografi 13" class="gallery-img">
+                    <img src="assets/images/jepretan/_MG_5615 (1).jpg" alt="Fotografi 13" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 13</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/20161007144110__MG_5658.jpg" alt="Fotografi 14" class="gallery-img">
+                    <img src="assets/images/jepretan/20161007144110__MG_5658.jpg" alt="Fotografi 14" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 14</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/20161007144124__MG_5659.jpg" alt="Fotografi 15" class="gallery-img">
+                    <img src="assets/images/jepretan/20161007144124__MG_5659.jpg" alt="Fotografi 15" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 15</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/20161007150900__MG_5714.jpg" alt="Fotografi 16" class="gallery-img">
+                    <img src="assets/images/jepretan/20161007150900__MG_5714.jpg" alt="Fotografi 16" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 16</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/20161007151030__MG_5719.jpg" alt="Fotografi 17" class="gallery-img">
+                    <img src="assets/images/jepretan/20161007151030__MG_5719.jpg" alt="Fotografi 17" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 17</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/20161007151202__MG_5721 (2).jpg" alt="Fotografi 18" class="gallery-img">
+                    <img src="assets/images/jepretan/20161007151202__MG_5721 (2).jpg" alt="Fotografi 18" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 18</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/IMG_4018.jpg" alt="Fotografi 19" class="gallery-img">
+                    <img src="assets/images/jepretan/IMG_4018.jpg" alt="Fotografi 19" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 19</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/IMG_4033.jpg" alt="Fotografi 20" class="gallery-img">
+                    <img src="assets/images/jepretan/IMG_4033.jpg" alt="Fotografi 20" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 20</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/IMG_4034.jpg" alt="Fotografi 21" class="gallery-img">
+                    <img src="assets/images/jepretan/IMG_4034.jpg" alt="Fotografi 21" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 21</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/IMG_4038 (1).jpg" alt="Fotografi 22" class="gallery-img">
+                    <img src="assets/images/jepretan/IMG_4038 (1).jpg" alt="Fotografi 22" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 22</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/IMG_4263.jpg" alt="Fotografi 23" class="gallery-img">
+                    <img src="assets/images/jepretan/IMG_4263.jpg" alt="Fotografi 23" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 23</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/IMG_4301 (1).jpg" alt="Fotografi 24" class="gallery-img">
+                    <img src="assets/images/jepretan/IMG_4301 (1).jpg" alt="Fotografi 24" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 24</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/IMG_4307.jpg" alt="Fotografi 25" class="gallery-img">
+                    <img src="assets/images/jepretan/IMG_4307.jpg" alt="Fotografi 25" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 25</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/IMG_4317.jpg" alt="Fotografi 26" class="gallery-img">
+                    <img src="assets/images/jepretan/IMG_4317.jpg" alt="Fotografi 26" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 26</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/IMG_4318.jpg" alt="Fotografi 27" class="gallery-img">
+                    <img src="assets/images/jepretan/IMG_4318.jpg" alt="Fotografi 27" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 27</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/IMG_4323.jpg" alt="Fotografi 28" class="gallery-img">
+                    <img src="assets/images/jepretan/IMG_4323.jpg" alt="Fotografi 28" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 28</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/IMG_4332.jpg" alt="Fotografi 29" class="gallery-img">
+                    <img src="assets/jepretan/IMG_4332.jpg" alt="Fotografi 29" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 29</p>
                 </div>
                 <div class="gallery-card">
-                    <img src="assets/images/fotografi/IMG_4340.jpg" alt="Fotografi 30" class="gallery-img">
+                    <img src="assets/images/jepretan/IMG_4340.jpg" alt="Fotografi 30" class="gallery-img">
                     <p class="gallery-text">Dokumentasi Foto 30</p>
                 </div>
             </div>
